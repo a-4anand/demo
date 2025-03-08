4e19aac9b24700f30934f5e688ef9227cf6adedd
Anand kumar dubey <142786893+a-4anand@users.noreply.github.com>
2025-03-08 20:49:12 +0530

    last push second

--- demo/settings.py
+++ demo/settings.py
@@ -123,12 +123,3 @@ STRIPE_PUBLIC_KEY = 'pk_test_lX3r6OMjOU2yzFsNSHq6belT00EY82kZmH'
 STRIPE_SECRET_KEY = 'sk_test_tn0CTDaIJHUJyAqhsf39cfsC00LNjsqDnb'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ad3810242@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'tpjp nmim qnqo vxoa'  # The correct app password
-DEFAULT_FROM_EMAIL = 'AutoWorth Support <ad3810242@gmail.com>'
